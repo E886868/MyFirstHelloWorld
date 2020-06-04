47bc4814970db7cb1b714869c0778d68766f4564
Caroline Thomas <caroline_thomas_@hotmaill.co.uk>
2020-06-04 11:40:29 +0100

    strcpy and strcat

--- test.cxx
+++ test.cxx
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void red()
+/*enum colour
+{
+	red = "\033[1;31m",
+	green = "\033[0;32m",
+	yellow = "\033[0;33m",
+	blue = "\033[0;34m",
+	megenta = "\033[0;35m",
+	cyan = "\033[0;36m",
+	reset = "\033[0m"
+} colour;
+
+
+
+/*void red()
 {
 	printf("\033[1;31m");
 }
@@ -22,16 +35,21 @@ int main()
 	printf("Hello");
 	mag();
 	printf(" World\n");
-}
+}*/
 
-/*{
+
+int main()
+{
+	char  blue[50];
+
 	char str1[1];
 
-	strcpy(str1, red());
+	strcpy(str1,"\033[0;33m");
 
 	strcat(str1, "Hello World\n");
 
 	printf(str1);
 
    	return 0;
-}*/
+}
+# HelloWorld
